36bc6dacc6a03a0323489d4cbefcd5230639ac67
Jacob McCann <jacob.mccann2@target.com>
2016-02-11 14:17:01 -0600

    Add some testing for rendering of Caddyfile

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,13 +7,37 @@ require 'spec_helper'
 
 describe 'caddy::default' do
   context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
+    cached(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       chef_run # This should not raise an error
+    end
+
+    it 'creates Caddyfile' do
+      expect(chef_run).to create_template '/etc/Caddyfile'
+    end
+
+    context "node['caddy']['hosts'] is populated" do
+      cached(:chef_run) do
+        runner = ChefSpec::ServerRunner.new do |node, _server|
+          node.set['caddy']['hosts'] = {
+            'localhost:80' => {
+              'log' => 'localhost.log'
+            },
+            'localhost:8080' => {
+              'log' => 'localhost_alt.log'
+            }
+          }
+        end
+        runner.converge(described_recipe)
+      end
+
+      it 'populates Caddyfile with site entries' do
+        expect(chef_run).to render_file('/etc/Caddyfile').with_content "localhost:80  {\n  log localhost.log\n}\nlocalhost:8080  {\n  log localhost_alt.log\n}"
+      end
     end
   end
 end
