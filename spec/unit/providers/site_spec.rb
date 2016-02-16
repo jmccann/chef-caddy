d732a610a20219f4756a7e22d42e2f646b4a7f65
Jacob McCann <jacob.mccann2@target.com>
2016-02-16 10:20:09 -0600

    Check for proper indentation

--- spec/unit/providers/site_spec.rb
+++ spec/unit/providers/site_spec.rb
@@ -22,11 +22,11 @@ describe 'test::site' do
       end
 
       it 'contains to' do
-        expect(chef_run).to render_file('/etc/caddy/sites.d/basic_proxy.conf').with_content(/^[ ]*proxy .* localhost:8000 localhost:8001$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/basic_proxy.conf').with_content(/^  proxy .* localhost:8000 localhost:8001$/)
       end
 
       it 'does not have extras' do
-        expect(chef_run).not_to render_file('/etc/caddy/sites.d/basic_proxy.conf').with_content(/^[ ]*proxy .* {$/)
+        expect(chef_run).not_to render_file('/etc/caddy/sites.d/basic_proxy.conf').with_content(/^  proxy .* {$/)
       end
     end
 
@@ -36,11 +36,11 @@ describe 'test::site' do
       end
 
       it 'contains to arg' do
-        expect(chef_run).to render_file('/etc/caddy/sites.d/proxy_arg_block.conf').with_content(/^[ ]*proxy .* localhost:8000 localhost:8001 {$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/proxy_arg_block.conf').with_content(/^  proxy .* localhost:8000 localhost:8001 {$/)
       end
 
       it 'contains extras from block' do
-        expect(chef_run).to render_file('/etc/caddy/sites.d/proxy_arg_block.conf').with_content(/^[ ]*max_fails 10$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/proxy_arg_block.conf').with_content(/^    max_fails 10$/)
       end
     end
 
@@ -50,13 +50,13 @@ describe 'test::site' do
       end
 
       it 'contains to' do
-        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^[ ]*proxy .* localhost:8000 {$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^  proxy .* localhost:8000 {$/)
       end
 
       it 'contains proxy_headers' do
-        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^[ ]*proxy_header X-Forwarded-Proto {scheme}$/)
-        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^[ ]*proxy_header X-Forwarded-For {host}$/)
-        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^[ ]*proxy_header Host {host}$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^    proxy_header X-Forwarded-Proto {scheme}$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^    proxy_header X-Forwarded-For {host}$/)
+        expect(chef_run).to render_file('/etc/caddy/sites.d/advanced_proxy.conf').with_content(/^    proxy_header Host {host}$/)
       end
     end
   end
